f3381357ca720f424e76c0d41090842ff0ef5195
Eden Sahilé <edensahile28@gmail.com>
2022-04-10 19:01:50 +0200

    hard data insertion

---
+++ src/components/Experience/Experience.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+export default class Experience extends Component {
+  render() {
+    return (
+      <section class="resume-section p-3 p-lg-5 d-flex justify-content-center" id="experience">
+        <div class="w-100">
+          <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+            <div class="resume-content">
+              <h3 class="mb-0">Web Developer</h3>
+              <div class="subheading mb-3">Intelitec Solutions</div>
+              <p>description: 'Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.',</p>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">March 2013 - Present</span>
+            </div>
+          </div>
+        </div>
+
+      </section>)
+  }
+}
